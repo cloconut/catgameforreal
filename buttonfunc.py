747164322a4e78eeace020f6e582bd2fc798001e
Clodagh Campbell <campbellc@macstudent.com.au>
2025-05-16 16:21:35 +1000

    button function seperated from main code

---
+++ buttonfunc.py
@@ -0,0 +1,29 @@
+class Button(): # baraltech YT tutorial :)
+    def __init__(self, image, pos, textinput, font, colour, hovcolour):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font # text font
+        self.colour, self.hovcolour = colour, hovcolour # default / hover button colour
+        self.textinput = textinput # text in button
+        self.text = self.font.render(self.textinput, True, self.colour)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def checkforinput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
+
+    def changecolour(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.textinput, True, self.hovcolour)
+        else:
+            self.text = self.font.render(self.textinput, True, self.colour)
